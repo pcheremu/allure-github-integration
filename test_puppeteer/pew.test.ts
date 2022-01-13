8aa229353e208fbbbaae0c01529aaf633cfe434b
warsmit <sazhin.maksim.s@gmail.com>
2022-01-13 14:04:12 +0300

    update

--- test_puppeteer/pew.test.ts
+++ test_puppeteer/pew.test.ts
@@ -2,7 +2,18 @@ import puppeteer from 'puppeteer';
 
 describe("test", () => {
   it('Test run settings', async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+      {
+        headless: false,
+        slowMo: 5,
+        args: [
+          //'--start-maximized',
+          `--no-sandbox`,
+          `--disable-setuid-sandbox`,
+          `--window-size=1920,1080`
+        ],
+      }
+    );
     const page = await browser.newPage();
     await page.goto('https://example.com');
     await page.screenshot({ path: 'test_puppeteer/artifacts/example.png' });
