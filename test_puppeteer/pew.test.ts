ef93b0dbb096676ed073a54fd2aa62f03f88147c
pcheremu <51121511+pcheremu@users.noreply.github.com>
2022-01-28 04:04:14 +0300

    Added markup for Allure
    https://github.com/allure-examples/allure-typescript-mocha/blob/master/specs/step.spec.ts

--- test_puppeteer/pew.test.ts
+++ test_puppeteer/pew.test.ts
@@ -1,20 +1,51 @@
 import puppeteer from 'puppeteer';
+import { allure } from "allure-mocha/runtime";
+import { expect } from "chai";
 
-describe("test", () => {
+const commonStep = () =>
+  allure.step("External step", () => {
+    allure.step("Child step for External step", () => {
+      expect(false).to.equal(true);
+    });
+  });
+
+describe("Allure step", () => {
   it('Test run settings', async () => {
-    const browser = await puppeteer.launch(
-      {
-        headless: false,
-        slowMo: 5,
-        args: [
-          `--no-sandbox`,
-          `--disable-setuid-sandbox`,
-        ],
-      }
-    );
-    const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({ path: 'test_puppeteer/artifacts/example.png' });
-    await browser.close();
+    allure.step("First step - puppeteer.launch", () => {
+      const browser = await puppeteer.launch(
+        {
+          headless: false,
+          slowMo: 5,
+          args: [
+            `--no-sandbox`,
+            `--disable-setuid-sandbox`,
+          ],
+        }
+      );
+    });
+    allure.step("browser.newPage", () => {
+      const page = await browser.newPage();
+    });
+    allure.step("goto example.com ", () => {
+      await page.goto('https://example.com');
+    });
+    allure.step("page.screenshot", () => {
+      await page.screenshot({ path: 'test_puppeteer/artifacts/example.png' });
+    });
+    allure.step("browser.close", () => {
+      await browser.close();
+    });
   }, 15000);
+  
+  it("allows nested steps", () => {
+    allure.step("Parent step", () => {
+      allure.step("Child step", () => {
+        expect(true).to.equal(true);
+      });
+    });
+  });
+  
+  it("helpful for highlighting common test code usage", () => {
+    commonStep();
+  });
 });
